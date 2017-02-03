665b771591d58b76243b1430212b35d3e0a025c3
ComLin010 <superlin_4025@qq.com>
2017-02-04 00:49:16 +0800

    手机发送信息获取成功

--- Spring_Wifi/Spring_Wifi.ino
+++ Spring_Wifi/Spring_Wifi.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+#include <String.h>
 
 
 SoftwareSerial mySerial(10, 11); // RX, TX
@@ -6,7 +7,7 @@ SoftwareSerial mySerial(10, 11); // RX, TX
 void setup()
 {
   // Open serial communications and wait for port to open:
-  Serial.begin(115200);
+  Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
@@ -14,7 +15,7 @@ void setup()
   Serial.println("Goodnight moon!");
 
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(115200);
+  mySerial.begin(9600);
   mySerial.println("Hello, world?");
   //开启多连接模式命令
   mySerial.println("AT+CIPMUX=1");
@@ -22,12 +23,41 @@ void setup()
   delay(2000);
   mySerial.println("AT+CIPSERVER=1,8080");
   delay(3000);
+  Serial.print("setp");
 }
 
 void loop() // run over and over
 {
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
+  String data = "";
+  if (mySerial.available() > 0) {
+
+    unsigned long start;
+    start = millis();
+
+    while (millis() - start < 100)
+    {
+      if (mySerial.available() > 0)
+      {
+        char sjesh = mySerial.read();
+        data += sjesh;
+        //          Serial.print(data.indexOf("\nOK"));
+      }
+      if (data.indexOf("\nOK") != -1)
+      {
+        break;
+      }
+
+    }
+    //      截取字符串
+    if (data.indexOf(":") > 0)
+    {
+      //          Serial.print(data.indexOf(":")); 获取符号下标
+      //          Serial.print(data.length()); 获取字符串长度
+      //      Serial.print(data.substring(data.indexOf(":") + 1, data.length()));从符号下标加1位截取
+      String sge = data.substring(data.indexOf(":") + 1, data.length());
+      if (sge == "on") {
+        Serial.print("\nok");
+      }
+    }
+  }
 }
